649124ef9b4796972285a10868ed5b91bcafa457
salahjaouni <salahjaouni@gmail.com>
2022-10-05 15:32:53 +0200

    ILU1LesGaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,34 @@
+package personnages;
+
+public class Gaulois {
+	private static String nom;
+	private static int force;
+	private int effetPotion = 1;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+				+ romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+	private static void asterix(String nom, int force) {
+		char[] asterix = null;{
+		asterix (nom = "Astérix", force = 8);
+		System.out.println(asterix);
+
+		
+	}
+	}
+}
